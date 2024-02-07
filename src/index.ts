1895be8e2b1fd61a1c4ead2288e40f2f6cd0773b
ChsBuffer <33744752+chsbuffer@users.noreply.github.com>
2024-02-07 14:14:09 +0800

    compare version

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import TelegramBot from "./telegram_bot";
 import { apps, make_body } from "./omaha";
-
-const array = require("lodash/array");
-const string = require("lodash/string");
+import { zip, escape } from "lodash-es";
+import { compareVer } from "./parse-version";
 
 class JsonResponse extends Response {
 	constructor(body?: any, init?: ResponseInit | Response) {
@@ -30,36 +29,56 @@ function apply<T, S>(value: T, expr: (obj: T) => S): S {
 }
 
 async function make_message(bot: TelegramBot, env: Environment, app: any, app_fallback: any) {
+	// this function weaving a Telegram HTML message from the `app`,
+	// send to the telegram session specified by CHAT_ID,
+	// and update the version string saved in Cloudflare KV
+
 	const appid = app.appid;
+	const current_version = (await env.KV.get(appid)) ?? "0.0.0.0";
+
 	const info = apps[appid];
 	const appname = info.name ?? appid;
-
 	if (app.updatecheck.status !== "ok") {
-		console.log(`No update for ${appname}`);
+		// "ok": An update is available and should be applied.
+		console.log(`No update for "${appname}" v${current_version}`);
 		return;
 	}
 
-	let msg: string[] = [];
-
-	var updatecheck = app.updatecheck;
-	const version = updatecheck.manifest.version;
+	const version = app.updatecheck.manifest.version;
+	console.log(`found update for "${appname}" v${current_version}: v${version}`);
+	// select app_fallback when two versions are the same.
+	// this is because we don't like the incremental update package from `app`.
+	var updatecheck: any;
 	if (app_fallback?.updatecheck?.manifest?.version === version) {
-		// assume that when the versions are the same, then they are the same build.
 		updatecheck = app_fallback.updatecheck;
+	} else {
+		updatecheck = app.updatecheck;
 	}
 
+	// when server responsed version is lower than KV saved version ... fuck google.
+	if (compareVer(version, current_version) < 0) {
+		console.log(
+			`Server responsed "${appname}" v${version} is older than current version v${current_version}, ignored.`
+		);
+		return;
+	}
+
+	// #region weaving telegram html message
+
+	let msg: string[] = [];
+	// Product and version
 	msg.push(`${appname} <code>${version}</code>\n`);
 	if (info.tag) {
 		msg.push(`#${info.tag}\n`);
 	}
 	msg.push("\n");
 
+	// Update Channel Name
 	msg.push(`Channel: ${app.cohortname}\n`);
-	// Link
+	// Download Link
 	const url = apply(
 		updatecheck.urls.url,
-		urls =>
-			urls.find((s: any) => s.codebase.startsWith("https://dl.google.com")) ?? urls.at(-1),
+		urls => urls.find((s: any) => s.codebase.startsWith("https://dl.google.com")) ?? urls.at(-1)
 	);
 	const urlbase = url.codebase;
 	const pkg = updatecheck.manifest.packages.package.at(-1);
@@ -67,20 +86,19 @@ async function make_message(bot: TelegramBot, env: Environment, app: any, app_fa
 
 	// Arguments
 	if (updatecheck.manifest.arguments) {
-		msg.push(
-			`Arguments: <code>${string.escape(updatecheck.manifest.arguments)}</code>\n`,
-		);
+		msg.push(`Arguments: <code>${escape(updatecheck.manifest.arguments)}</code>\n`);
 	}
 
-	// SHA256, Size
+	// SHA256, Download Size
 	msg.push(
-		`SHA256: <code>${pkg.hash_sha256}</code>\nSize: ${(
-			parseInt(pkg.size) /
-			1024 ** 2
-		).toFixed(2)} MiB`,
+		`SHA256: <code>${pkg.hash_sha256}</code>\nSize: ${(parseInt(pkg.size) / 1024 ** 2).toFixed(
+			2
+		)} MiB`
 	);
 
-	// send message
+	//#endregion
+
+	// send message to telegram
 	const message = "".concat(...msg);
 	let res = info.ogimg
 		? await bot.sendPhoto(env.CHAT_ID, info.ogimg, message)
@@ -90,6 +108,7 @@ async function make_message(bot: TelegramBot, env: Environment, app: any, app_fa
 		return;
 	}
 
+	// Upload latest version number to Cloudflare KV
 	await env.KV.put(appid, version);
 }
 
@@ -118,7 +137,9 @@ export default {
 		});
 	},
 
-	async scheduled(request: Request, env: Environment) {
+	async scheduled(event: ScheduledController, env: Environment) {
+		console.log(`schedule triggered: UTC ${new Date(event.scheduledTime).toISOString()}`);
+
 		let api = env.ENVIRONMENT == "dev" ? "http://localhost:8081" : undefined;
 		const bot = new TelegramBot(env.BOT_TOKEN, api);
 
@@ -141,25 +162,24 @@ export default {
 		});
 		if (!resp_fallback.ok) {
 			console.log(
-				`post update2 (fallback) failed: ${resp_fallback.status} ${resp_fallback.statusText}`,
+				`post update2 (fallback) failed: ${resp_fallback.status} ${resp_fallback.statusText}`
 			);
 			return;
 		}
 		let update2_fallback: any = JSON.parse((await resp_fallback.text()).substring(5));
 
-		for (const apps_and_fallback of array.zip(
-			update2.response.app,
-			update2_fallback.response.app,
-		) as [[any, any]]) {
+		for (const apps_and_fallback of zip(update2.response.app, update2_fallback.response.app) as [
+			[any, any]
+		]) {
 			try {
 				await make_message(bot, env, ...apps_and_fallback);
-			} catch (e) {
+			} catch (e: any) {
 				console.log(e);
 				await bot.sendMessage(
 					env.OWNER_ID,
-					`<code>${string.escape(e instanceof Error ? e.stack : e)}</code>\n\n<code>${string.escape(
-						respText,
-					)}</code>`,
+					`<code>${escape(e instanceof Error ? e.stack : e)}</code>\n\n<code>${escape(
+						respText
+					)}</code>`
 				);
 			}
 		}
---
+++ src/parse-version.ts
@@ -0,0 +1,40 @@
+interface Version {
+	major: number;
+	minor: number;
+	patch: number;
+	build: number;
+}
+
+function parseVersion(versionString: string): Version {
+	const vparts: string[] = versionString.split(".");
+	if (vparts.length !== 4) {
+		throw new Error("Version format isn't valid (must be `*.*.*.*`)");
+	} else {
+		const major: number = parseInt(vparts[0]);
+		const minor: number = parseInt(vparts[1]);
+		const patch: number = parseInt(vparts[2]);
+		const build: number = parseInt(vparts[3]);
+
+		if (isNaN(major) || isNaN(minor) || isNaN(patch) || isNaN(build)) {
+			throw new Error("Version format isn't valid (must be `*.*.*.*`)");
+		}
+
+		return { major, minor, patch, build };
+	}
+}
+
+function compareVer(v1: string, v2: string): number {
+	let thiz = parseVersion(v1);
+	let that = parseVersion(v2);
+	if (thiz.major != that.major) {
+		return thiz.major - that.major;
+	} else if (thiz.minor !== that.minor) {
+		return thiz.minor - that.minor;
+	} else if (thiz.patch !== that.patch) {
+		return thiz.patch - that.patch;
+	} else {
+		return thiz.build - that.build;
+	}
+}
+
+export { parseVersion, Version, compareVer };
